26747a8f2a6678ef7594222c4086c81124361fda
Miguel Nieto A <anietom@uninorte.edu.co>
2021-05-27 12:56:15 -0500

    build finished

---
+++ build/precache-manifest.646dd36466f6f47bf34ac0622d6a22ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d337de0788daa110b05488f192f2d9",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "42951e09283e7bdb0324",
+    "url": "/material-dashboard-react/static/css/2.95c27a86.chunk.css"
+  },
+  {
+    "revision": "79c6d9a3c779f39a66d4",
+    "url": "/material-dashboard-react/static/css/main.0ac1ac59.chunk.css"
+  },
+  {
+    "revision": "42951e09283e7bdb0324",
+    "url": "/material-dashboard-react/static/js/2.9094bcab.chunk.js"
+  },
+  {
+    "revision": "a1d201903f5730a17e806dde39d93552",
+    "url": "/material-dashboard-react/static/js/2.9094bcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c6d9a3c779f39a66d4",
+    "url": "/material-dashboard-react/static/js/main.97ad741d.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.97ad741d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
